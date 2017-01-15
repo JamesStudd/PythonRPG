3f11158b069f9e7de9af4c75d78b9336dfe59e5f
JamesStudd <james.studd@live.co.uk>
2017-01-15 16:35:56 +0000

    Added a few temp buttons
    Inventory, save, load, options and quit have all been added as temporary buttons. These will have update and draw methods attached to them

--- Data/pause_menu.py
+++ Data/pause_menu.py
@@ -6,6 +6,22 @@ pg.init()
 def draw_pause_menu(surface):
 
     surface.blit(IMAGES['pause_menu'], (200, 200))
-    r = RpgButton(200, 200)
-    r.draw(surface, 'temp_button')
-    r.update(pg.mouse.get_pos())
+    list_of_buttons = []
+    inventory_button = RpgButton(300, 300, 'temp_button_inventory')
+    list_of_buttons.append(inventory_button)
+
+    save_button = RpgButton(300, 340, 'temp_button_save')
+    list_of_buttons.append(save_button)
+
+    load_button = RpgButton(300, 380, 'temp_button_load')
+    list_of_buttons.append(load_button)
+
+    options_button = RpgButton(300, 420, 'temp_button_options')
+    list_of_buttons.append(options_button)
+
+    quit_button = RpgButton(300, 460, 'temp_button_quit')
+    list_of_buttons.append(quit_button)
+
+    for b in list_of_buttons:
+        b.draw(surface)
+        b.update(pg.mouse.get_pos())
--- Data/rpg_button.py
+++ Data/rpg_button.py
@@ -10,14 +10,16 @@ class RpgButton():
     y = 0
     image = None
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, image):
         self.x = x
         self.y = y
-
-    def draw(self, surface, image):
-        self.surface = surface
         self.image = image
+
+    def draw(self, surface):
+        self.surface = surface
         self.surface.blit(IMAGES[self.image], (self.x, self.y))
 
     def update(self, pos):
-        print(pos)
+        if pg.mouse.get_pressed()[0]:
+            if all((pos[0] >= self.x, pos[0] <= self.x + 100, pos[1] >= self.y, pos[1] <= self.y + 30)):
+                print("it happened")
